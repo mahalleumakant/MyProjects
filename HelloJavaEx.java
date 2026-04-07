53aa95ace998a94d960231645d8c52e4cb565133
=Umakant Mahalle <umakantmahalle@gmail.com>
2026-04-07 18:16:58 +0000

    I have added helloworld of python and java

---
+++ HelloJavaEx.java
@@ -0,0 +1,5 @@
+class HelloJavaEx {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ helloworld.py
@@ -0,0 +1,4 @@
+
+print ("Hello MAhalle patil this is new code updated")
+print ("Thoda update kiya hai kal , night ho gayi thi to practice rok di thi")
+
