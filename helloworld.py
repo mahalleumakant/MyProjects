31e620d3c1126aca308d751cc24c2e36e211d423
=Umakant Mahalle <umakantmahalle@gmail.com>
2026-04-13 19:58:05 +0000

    merge using cmd line

--- helloworld.py
+++ helloworld.py
@@ -4,5 +4,5 @@ print ("Thoda update kiya hai kal , night ho gayi thi to practice rok di thi")
 print ("ab ye humne naya branch bana k code update check karne k liye kiya hai ")
 print ("ab ye naya staging kar k banaya usme ek update karte hai , try kar k")
 print ("FINAL")
-
+print ("this is practice with command line to merge with main")
 
