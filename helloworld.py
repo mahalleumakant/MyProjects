2f04b8221d7984e950ca3906e9e2e7d5603db646
EC2 Default User <ec2-user@ip-172-31-43-162.ap-south-1.compute.internal>
2026-05-11 14:47:54 +0000

    conflict practice

--- helloworld.py
+++ helloworld.py
@@ -2,4 +2,8 @@ print ("Hello World welcome to the Umakant Git Practice")
 print ("1st update")
 print ("second update")
 print ("Third update")
+<<<<<<< HEAD
 print ("To check git conflict remotely")
+=======
+print ("to check git conflict states")
+>>>>>>> b93c5cd (conflict)
